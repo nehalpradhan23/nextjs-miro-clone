9d436647100088c6abe73704e59fe020b1687691
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-04-04 04:15:47 +0530

    basic layout - sidebar, navbar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-y-4">
       <div className="">authenticated</div>
