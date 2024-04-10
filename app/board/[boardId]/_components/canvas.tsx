aca631cae05ae87a99a954f9e0296675a1a4e32a
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-04-10 07:42:01 +0530

    canvas room info menu

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { useSelf } from "@/liveblocks.config";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -10,12 +8,9 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info);
-  console.log(info);
-
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
