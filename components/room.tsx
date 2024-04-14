67b13929b1e6bd31c644ed6528bde18fc693a28c
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-04-14 08:47:41 +0530

    insert layers (rectangle)

--- components/room.tsx
+++ components/room.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { RoomProvider } from "@/liveblocks.config";
+import { Layer } from "@/types/canvas";
+import { LiveMap, LiveObject, LiveList } from "@liveblocks/client";
+
 import { ClientSideSuspense } from "@liveblocks/react";
 import { ReactNode } from "react";
 
@@ -15,6 +18,11 @@ export const Room = ({ children, roomId, fallback }: RoomProps) => {
       id={roomId}
       initialPresence={{
         cursor: null,
+        selection: [],
+      }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
       }}
     >
       <ClientSideSuspense fallback={fallback}>
